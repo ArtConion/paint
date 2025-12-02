12072c9814f09ca7b066f77c086fd12038fe661b
ArtConion <artiom0408@mail.ru>
2025-12-02 09:38:21 +0300

    v1

--- main.cpp
+++ main.cpp
@@ -1,2 +1,22 @@
+#include <iostream>
+namespace topit {
+   struct p_t{
+    int x,y;
+  };
+  bool operator==(p_t a, p_t b)
+  bool operator!=(p_t a, p_t b)
+}
 int main()
-{}
+{
+  using namespace topit;
+  p_t a{1,1}, b{1,0};
+  std::cout << (a==b) << '\n';
+}
+bool topit::operator==(p_t a, p_t b)
+{
+  return a.x==b.x && a.y==b.y;
+}
+bool topit::operator!=(p_t a, p_t b)
+{
+  return !(a==b);
+}
